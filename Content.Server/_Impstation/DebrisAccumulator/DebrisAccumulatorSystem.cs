8028640a41aebec505d34e553c582edbcc134a37
Ruddygreat <ruddygreat1@gmail.com>
2024-10-20 15:06:24 +0100

    should've written this down before doing the last commit

--- Content.Server/_Impstation/DebrisAccumulator/DebrisAccumulatorSystem.cs
+++ Content.Server/_Impstation/DebrisAccumulator/DebrisAccumulatorSystem.cs
@@ -8,5 +8,7 @@ namespace Content.Server._Impstation.DebrisAccumulator;
 
 public sealed class DebrisAccumulatorSystem : EntitySystem
 {
-
+//I should probably write this down more, anyway
+//have a constant 1~2 min long timer that pulls in new wrecks when it ticks over, to make it not be a *constant* stream of wrecks & allow for some downtime
+//wreck group timer should be 5~7 mins
 }
